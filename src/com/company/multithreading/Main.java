2fc86b20f38a7a0d724a2c04a5425dc8f6d7659c
Constantine Solovev <trash.rico@gmail.com>
2014-12-20 20:00:46 +0400

    first commit

--- src/com/company/multithreading/Main.java
+++ src/com/company/multithreading/Main.java
@@ -6,25 +6,6 @@ public class Main {
 
         System.out.println("Start");
 
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                Thread currentThread = Thread.currentThread();
-//                String threadName = currentThread.getName();
-//                System.out.printf("%s start \n", threadName );
-//
-//                while (true) {
-//                    System.out.printf("%s thread working \n", threadName );
-//                    try {
-//                        Thread.sleep(1000);
-//                    } catch (InterruptedException e) {
-//                        System.out.printf("%s stopped \n", threadName );
-//                    }
-//                }
-//
-//            }
-//        }).start();
-
         new MyThread(" 1");
         new MyThread("  2");
         new MyThread("   3");
